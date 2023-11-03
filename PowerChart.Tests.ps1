59419af6e7b71af767dfaedf3c8e19510c48868e
carrvo <carrvo@gmail.com>
2023-11-02 21:41:30 -0600

    first attempt at InputObject+Properties

--- PowerChart.Tests.ps1
+++ PowerChart.Tests.ps1
@@ -7,12 +7,20 @@ Describe "PowerChart API" {
 	}
 	It "Shows Points" {
 		$chart = New-Chart
-		Add-Scatter -Chart $chart -XCoordinate 10 -YCoordinate 50
-		Add-Scatter -Chart $chart -XCoordinate 20 -YCoordinate 100
-		Add-Scatter -Chart $chart -XCoordinate 30 -YCoordinate 50
 		$chart.Title = 'Shows Points'
 		$chart.XAxisLabel = 'time'
 		$chart.YAxisLabel = 'Energy'
+		Add-Scatter -Chart $chart -XCoordinate 10 -YCoordinate 50
+		Add-Scatter -Chart $chart -XCoordinate 20 -YCoordinate 100
+		Add-Scatter -Chart $chart -XCoordinate 30 -YCoordinate 50
+		Show-Chart -Chart $chart
+	}
+	It "Draws a Scatter Chart" {
+		$chart = New-Chart
+		$chart.Title = 'Get-Process'
+		$chart.XAxisLabel = 'Process ID'
+		$chart.YAxisLabel = 'CPU'
+		Get-Process | Add-Scatter $chart -XProperty Id -YProperty CPU
 		Show-Chart -Chart $chart
 	}
 }
--- PowerChart/AddScatterCommand.cs
+++ PowerChart/AddScatterCommand.cs
@@ -28,10 +28,64 @@ namespace PowerChart
         [ValidateNotNull]
         public Int32 YCoordinate { get; set; }
 
+        [Parameter(Mandatory = true, ValueFromPipeline = true, ParameterSetName = "Properties")]
+        [ValidateNotNull]
+        public PSObject InputObject { get; set; }
+
+        [Parameter(Mandatory = true, Position = 1, ParameterSetName = "Properties")]
+        [ValidateNotNull]
+        public String XProperty { get; set; }
+
+        [Parameter(Mandatory = true, Position = 2, ParameterSetName = "Properties")]
+        [ValidateNotNull]
+        public String YProperty { get; set; }
+
         /// <inheritdoc/>
         protected override void ProcessRecord()
         {
-            Chart?.AddScatterPoint(XCoordinate, YCoordinate);
+            try
+            {
+                switch (ParameterSetName)
+                {
+                    case "Properties":
+                        XCoordinate = CastProperty(InputObject.Properties.Single(x => x.Name == XProperty));
+                        YCoordinate = CastProperty(InputObject.Properties.Single(x => x.Name == YProperty));
+                        break;
+                    default:
+                        break;
+                }
+                Chart?.AddScatterPoint(XCoordinate, YCoordinate);
+            }
+            catch (ArgumentException ex)
+            {
+                WriteError(new ErrorRecord(
+                    ex,
+                    "Casting InputObject Property",
+                    ErrorCategory.ParserError,
+                    InputObject
+                ));
+            }
+        }
+
+        private Int32 CastProperty(PSPropertyInfo property)
+        {
+            if (property.TypeNameOfValue == typeof(Int32).FullName)
+            {
+                return (Int32)property.Value;
+            }
+            else if (property.Value is PSObject psInt && psInt.BaseObject.GetType().FullName == typeof(Int32).FullName)
+            {
+                return (Int32)psInt.BaseObject;
+            }
+            else if (property.Value is PSObject psDouble && psDouble.BaseObject.GetType().FullName == typeof(Double).FullName)
+            {
+                Double d = (Double)psDouble.BaseObject;
+                return (Int32)d;
+            }
+            else
+            {
+                throw new ArgumentException($"Property value of {property.Name} is unsupported type {property.TypeNameOfValue} ({property.Value?.GetType()?.FullName}).");
+            }
         }
     }
 }

d8a511657ac04f1ee1f70c6e9f5091fa0ec41617
carrvo <carrvo@gmail.com>
2023-11-14 13:47:08 -0700

    #Requires will prevent execution if the module has any load error; however, it gives a misleading message

--- PowerChart.FunctionalTests/PowerChart.Tests.ps1
+++ PowerChart.FunctionalTests/PowerChart.Tests.ps1
@@ -1,4 +1,4 @@
-Import-Module $PSScriptRoot\..\bin\Debug\net6.0-windows\PowerChart.psd1
+#Requires -Module ..\bin\Debug\net6.0-windows\PowerChart.psd1
 
 Describe "PowerChart API" {
 	It "Shows Chart" {
